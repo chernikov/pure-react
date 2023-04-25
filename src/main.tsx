a4ec778a8e344e1ee748872f571924d4d41202c1
ypernarovskyi@softjourn.com <ypernarovskyi@softjourn.com>
2023-04-25 12:57:51 +0300

    added folder structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './app';
+import App from './components/app';
 ReactDOM.render(<App />, document.getElementById('root'));
